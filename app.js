f2e28684992d2f4dc6744ab3ab18b5ef9b04f2fc
Brad Bryant <bradleybryant86@gmail.com>
2021-01-18 00:15:33 -0600

    Created drawBoard function for styling boxes

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ console.log(boxes);
 const drawBoard = () => {
     boxes.forEach((box, index) => {
         let styleString = '';
-        //boxes on top
+        //top row: 0, 1, 2
         if (index < 3) {
-            styleString += `border-bottom: 3px solid var(--purple)`;
+            styleString += `border-bottom: 3px solid var(--purple);`;
         }
-        //boxes on left
-        if (index % 3) {
-            styleString += `border-right: 3px solid var(--purple)`;
-
+        //left column: 0, 3, 6
+        if (index % 3 === 0) {
+            styleString += `border-right: 3px solid var(--purple);`;
         }
+        //right column: 2, 5, 8
+        if (index % 3 === 2) {
+            styleString += `border-left: 3px solid var(--purple);`;
+        }
+        //bottom row: 6, 7, 8
+        if (index > 5) {
+            styleString += `border-top: 3px solid var(--purple);`;
+        }
+        box.style = styleString;
     })
 }
+
+drawBoard();
