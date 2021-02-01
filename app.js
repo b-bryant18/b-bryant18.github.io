73035ff9cfed92f31370771ef9b30e92a34e2f03
Brad Bryant <bradleybryant86@gmail.com>
2021-02-01 00:09:37 -0600

    Changed purple UI to dodger blue UI

--- app.js
+++ app.js
@@ -15,19 +15,19 @@ const drawBoard = () => {
         let styleString = '';
         //top row: 0, 1, 2
         if (index < 3) {
-            styleString += `border-bottom: 3px solid var(--purple);`;
+            styleString += `border-bottom: 3px solid var(--blue);`;
         }
         //left column: 0, 3, 6
         if (index % 3 === 0) {
-            styleString += `border-right: 3px solid var(--purple);`;
+            styleString += `border-right: 3px solid var(--blue);`;
         }
         //right column: 2, 5, 8
         if (index % 3 === 2) {
-            styleString += `border-left: 3px solid var(--purple);`;
+            styleString += `border-left: 3px solid var(--blue);`;
         }
         //bottom row: 6, 7, 8
         if (index > 5) {
-            styleString += `border-top: 3px solid var(--purple);`;
+            styleString += `border-top: 3px solid var(--blue);`;
         }
         box.style = styleString;
         box.addEventListener('click', boxClicked)
